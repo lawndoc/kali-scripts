b8c50d72d0a418e9dcc284485e78bb63ec3aedf7
root <root@localhost.localdomain>
2019-07-20 23:37:51 -0400

    fixed install script

--- install.sh
+++ install.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 
-for file in .; do
-  if [ $file -eq "install.sh" ]; then
+for file in *; do
+  if [ $file == "install.sh" ]; then
     continue
   fi
   ln $(pwd)/$file /usr/bin/$file
