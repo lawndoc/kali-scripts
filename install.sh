bf41d78412a17bba41983bc9ec86a752fe51753c
root <kali@kali.com>
2019-07-21 00:31:08 -0400

    added subfolder recursion

--- install.sh
+++ install.sh
@@ -1,8 +1,22 @@
 #!/usr/bin/env bash
 
-for file in *; do
-  if [ $file == "install.sh" ]; then
-    continue
-  fi
-  ln $(pwd)/$file /usr/bin/$file
-done
+install_from () {
+  for file in $1/*; do
+
+    # recursively add files in folders
+    if [ -d $file ]; then
+      install_from $file
+
+    # skip this script
+    elif [ $file == "$1/install.sh" ]; then
+      continue
+
+    # hard link each script to usr bin
+    elif [ -f $file ]; then
+      ln $file /usr/bin/$(basename $file)
+
+    fi
+  done
+}
+
+install_from $(pwd)
