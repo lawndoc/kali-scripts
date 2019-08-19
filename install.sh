c4954ab2783a47e8d0fa12128612791506c8f018
C.J. May <doctormay6@gmail.com>
2019-08-19 16:26:23 -0500

    fixed install script

--- install.sh
+++ install.sh
@@ -5,14 +5,17 @@ install_scripts () {
 
     # skip this script and the man pages
     if [ $file == "$1/install.sh" ] || [ $file == "$1/Man" ]; then
+      echo skipping $file
       continue
 
     # recursively add files in folders
     elif [ -d $file ]; then
+      echo entering directory $file
       install_scripts $file
 
     # hard link each script to usr bin
     elif [ -f $file ]; then
+      echo attempting to install $file
       ln $file /usr/bin/$(basename $file)
 
     fi
@@ -20,18 +23,20 @@ install_scripts () {
 }
 
 install_man_pages () {
-  for file in Man/*; do
+  for file in ./Man/*; do
+    filename="${file##*/}"
 
     # skip the new man page creator script
-    if [ $file == "newPage.py" ]; then
+    if [ $filename == "newPage.py" ]; then
       continue
 
     # install man page
     else
-      install -g 0 -o 0 -m 0644 $file.1 /usr/local/man/man1
-      gzip /usr/local/man1/$file.1
+      install -g 0 -o 0 -m 0644 $file /usr/local/man/man1
+      gzip /usr/local/man/man1/$filename
     fi
   done
+  mandb
 }
 
 install_scripts $(pwd)
